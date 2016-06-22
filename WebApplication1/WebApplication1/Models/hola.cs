d72590614887734ceb189ef729d90410de4a9896
ezedev <ezequiel.ivan.guzman@gmail.com>
2016-06-22 00:13:48 -0300

    prueba
    prueba

---
+++ WebApplication1/WebApplication1/Models/hola.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class hola
+    {
+        public int a;
+    }
+}
